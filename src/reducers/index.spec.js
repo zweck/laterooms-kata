857aaae7de7a4f0e86a56a82b9ffe43a85cb5666
Phil Hauser <phil@zweck.co>
2019-03-06 23:11:14 +0000

    Add more tests

--- src/reducers/index.spec.js
+++ src/reducers/index.spec.js
@@ -9,6 +9,14 @@ const GYM_STORE = [
   }
 ]
 
+const POOL_STORE = [
+  {
+    name: "hotelone",
+    starRating: 5,
+    facilities: ["car park", "pool"]
+  }
+]
+
 describe('reducer test', () => {
   it('should return default state when no action match', () => {
     expect(reducer(defaultState, { type: 'ACTION' })).toEqual(defaultState)
@@ -20,5 +28,21 @@ describe('reducer test', () => {
       { type: FILTER_BY_FACILITY, payload: ['gym'] }
     )
     expect(reducedState).toEqual(GYM_STORE)
+  })
+
+  it('should filter hotels by a multiple facilities', () => {
+    const reducedState = reducer(
+      defaultState,
+      { type: FILTER_BY_FACILITY, payload: ['car park', 'pool'] }
+    )
+    expect(reducedState).toEqual(POOL_STORE)
+  })
+
+  it('should return multiple hotels that share a facility', () => {
+    const reducedState = reducer(
+      defaultState,
+      { type: FILTER_BY_FACILITY, payload: ['car park'] }
+    )
+    expect(reducedState).toEqual([ ...POOL_STORE, ...GYM_STORE ])
   })
 })
