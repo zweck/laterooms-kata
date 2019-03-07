388f42f893a3bc256c415ec0b18da012ea6cfb1b
Phil Hauser <phil@zweck.co>
2019-03-07 10:40:31 +0000

    Hook up simple sorting to the view

--- src/App.js
+++ src/App.js
@@ -3,13 +3,15 @@ import { connect } from 'react-redux'
 import PropTypes from 'prop-types'
 import * as R from 'ramda'
 import { facilitiesToSelect } from './lib/uniq-facilities'
-import { filterByFacilities } from './actions'
+import { filterByFacilities, sortByStarAsc, sortByStarDesc } from './actions'
 import './App.css';
 
 import HotelCard from './widgets/hotel-card'
 import HotelSelector from './widgets/hotel-selector'
 
 export const App = ({
+  sortHotelsDesc,
+  sortHotelsAsc,
   filterOptions,
   hotels = [],
   facilities = [] }) => {
@@ -17,12 +19,17 @@ export const App = ({
     <div className="text-center flex flex-col items-center">
       <header className="bg-blue-light m-6 p-6 rounded shadow-lg max-w-sm">
         <h1 className="text-white text-3xl">Hotel finder</h1>
-        <span>
+        <div className='m-2'>
           <span className='text-white text-xl p-2'>Please select some facilities</span>
           <HotelSelector
             filterOptions={filterOptions}
             facilities={facilities} />
-        </span>
+          <div className='m-2'>
+            <span> Sort By Star </span>
+              <button onClick={sortHotelsAsc}>▲</button>
+              <button onClick={sortHotelsDesc}>▼</button>
+          </div>
+        </div>
       </header>
       <section className='result-list'>
         {R.map(hotel => (
@@ -34,6 +41,8 @@ export const App = ({
 }
 
 App.propTypes = {
+  sortHotelsDesc: PropTypes.func,
+  sortHotelsAsc: PropTypes.func,
   filterOptions: PropTypes.func,
   hotels: PropTypes.array,
   facilities: PropTypes.array
@@ -47,6 +56,8 @@ const mapStateToProps = ({
 })
 
 const mapDispatchToProps = dispatch => ({
+  sortHotelsDesc: R.compose(dispatch, sortByStarDesc),
+  sortHotelsAsc: R.compose(dispatch, sortByStarAsc),
   filterOptions: R.compose(dispatch, filterByFacilities)
 })
 
